7d78f8d5c9a51a3bdfe641159d3bea4e37549361
davidokun <davidokun99@gmail.com>
2020-07-16 21:01:24 +0200

    Create VPC and subnets

--- project-1/README.md
+++ project-1/README.md
@@ -7,3 +7,4 @@ terraform plan
 
 terraform apply
 
+terraform destroy
--- project-1/main.tf
+++ project-1/main.tf
@@ -8,4 +8,25 @@ provider "aws" {
 resource "aws_instance" "tf-project-1" {
   ami           = "ami-0ac80df6eff0e70b5"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "Project 1 HelloWorld"
+  }
+}
+
+resource "aws_vpc" "my-vpc-1" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "qa"
+  }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.my-vpc-1.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "qa-subnet"
+  }
 }
